b01ac1a82870387d2c05d18ca23babf11611d85a
aramirezdecruz3148 <leigh3148@gmail.com>
2019-10-14 10:03:39 -0700

    created my Overview component and a snapshot test

---
+++ src/components/Overview.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Overview() {
+  return (
+    <>
+      <h4>Overview</h4>
+      <p>
+        This is where I will explain the tech I used and the
+        reason for creating the project.
+      </p>
+    </>
+  );
+}
--- src/components/Title.js
+++ src/components/Title.js
@@ -7,3 +7,6 @@ export default function Title() {
     </header>
   );
 }
+//add some pictures and perhaps some decoration
+//look back at how I created the lettering for the simpsons build
+//the hey arnold text looks really similar
