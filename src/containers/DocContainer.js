97c0dbae54d5759e1986a2c1cbf218188083e3ce
aramirezdecruz3148 <leigh3148@gmail.com>
2019-10-14 19:38:04 -0700

    finished styling and got rid of Overview component because I realized I did not need it

--- src/containers/DocContainer.js
+++ src/containers/DocContainer.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Title from '../components/Title';
-import Overview from '../components/Overview';
 import CharacterRoutes from '../components/CharacterRoutes';
 import GifRoutes from '../components/GifRoutes';
 import Footer from '../components/Footer';
@@ -10,7 +9,6 @@ export default function DocContainer() {
   return (
     <div className={styles.background}>
       <Title />
-      <Overview />
       <CharacterRoutes />
       <GifRoutes />
       <Footer />
