0af8e0d335e2bceb3ef6cab1e72ca306afb30e77
aramirezdecruz3148 <leigh3148@gmail.com>
2019-10-14 19:34:05 -0700

    mostly finished styling the bulk of the routes

--- src/components/CharacterRoutes.js
+++ src/components/CharacterRoutes.js
@@ -10,44 +10,44 @@ export default function CharacterRoutes() {
     <div className={styles.flex}>
       <h3 className={styles.title}>Character Routes</h3>
       <section>
-        <h4>Get All Characters</h4>
-        <h5>https://hey-arnold-api.herokuapp.com/api/v1/characters</h5>
-        <p>
+        <h4 className={styles.queries}>Get All Characters</h4>
+        <h5 className={styles.endpoints}>https://hey-arnold-api.herokuapp.com/api/v1/characters</h5>
+        <p className={styles.description}>
           This route will return a paginated list.
           By default, a list (or <q>page</q>), will return 20 characters.
           To change this number, simply add a <q>perPage</q> query parameter,
           e.g. ?perPage=3. You can move to the next page of results by adding
           the <q>page</q> query parameter, e.g. ?page=2.
         </p>
-        <h5>{'https://hey-arnold-api.herokuapp.com/api/v1/characters?perPage=3&page=2'}</h5>
+        <h5 className={styles.endpoints}>{'https://hey-arnold-api.herokuapp.com/api/v1/characters?perPage=3&page=2'}</h5>
         <div className={styles.codeBlock}>
           <pre>{JSON.stringify(paginatedCharacters, null, 2)}</pre>
         </div>
-        <h5>Query by Character Name</h5>
-        <p>
+        <h4 className={styles.queries}>Query by Character Name</h4>
+        <p className={styles.description}>
           This route does allow you to query characters by name, with
           the ability to return all partial matches regardless of casing,
           e.g. ?name=hel.
         </p>
-        <h5>https://hey-arnold-api.herokuapp.com/api/v1/characters?name=hel</h5>
+        <h5 className={styles.endpoints}>https://hey-arnold-api.herokuapp.com/api/v1/characters?name=hel</h5>
         <div className={styles.codeBlock}>
           <pre>{JSON.stringify(nameCharacters, null, 2)}</pre>
         </div>
       </section>
       <section>
-        <h4>Get Random Character</h4>
-        <p>
+        <h4 className={styles.queries}>Get Random Character</h4>
+        <p className={styles.description}>
           This route returns one random character by default. To change this number, 
           simply add a <q>count</q> query parameter, e.g. ?count=2.
         </p>
-        <h5>https://hey-arnold-api.herokuapp.com/api/v1/characters/random?count=2</h5>
+        <h5 className={styles.endpoints}>https://hey-arnold-api.herokuapp.com/api/v1/characters/random?count=2</h5>
         <div className={styles.codeBlock}>
           <pre>{JSON.stringify(randomCharacter, null, 2)}</pre>
         </div>
       </section>
       <section>
-        <h4>Get Character by Id</h4>
-        <h5>https://hey-arnold-api.herokuapp.com/api/v1/characters/5da237699734fdcb7bef8f51</h5>
+        <h4 className={styles.queries}>Get Character by Id</h4>
+        <h5 className={styles.endpoints}>https://hey-arnold-api.herokuapp.com/api/v1/characters/5da237699734fdcb7bef8f51</h5>
         <div className={styles.codeBlock}>
           <pre>{JSON.stringify(characterById, null, 2)}</pre>
         </div>
--- src/components/GifRoutes.js
+++ src/components/GifRoutes.js
@@ -8,24 +8,24 @@ export default function GifRoutes() {
     <div className={styles.flex}>
       <h3 className={styles.title}>Gif Routes</h3>
       <section>
-        <h4>Get All GIFs</h4>
-        <h5>https://hey-arnold-api.herokuapp.com/api/v1/gifs</h5>
-        <p>This route will return a list of 49 Hey Arnold GIFs.</p>
+        <h4 className={styles.queries}>Get All GIFs</h4>
+        <h5 className={styles.endpoints}>https://hey-arnold-api.herokuapp.com/api/v1/gifs</h5>
+        <p className={styles.description}>This route will return a list of 49 Hey Arnold GIFs.</p>
       </section>
       <section>
-        <h4>Get Random GIF</h4>
-        <p>
+        <h4 className={styles.queries}>Get Random GIF</h4>
+        <p className={styles.description}>
           This route returns one random GIF by default. To change this number, 
           simply add a <q>count</q> query parameter, e.g. ?count=2.
         </p>
-        <h5>https://hey-arnold-api.herokuapp.com/api/v1/gifs/random?count=2</h5>
+        <h5 className={styles.endpoints}>https://hey-arnold-api.herokuapp.com/api/v1/gifs/random?count=2</h5>
         <div className={styles.codeBlock}>
           <pre>{JSON.stringify(randomGifs, null, 2)}</pre>
         </div>
       </section>
       <section>
-        <h4>Get GIF by Id</h4>
-        <h5>https://hey-arnold-api.herokuapp.com/api/v1/gifs/5da23754845fd2cb76d59ee7</h5>
+        <h4 className={styles.queries}>Get GIF by Id</h4>
+        <h5 className={styles.endpoints}>https://hey-arnold-api.herokuapp.com/api/v1/gifs/5da23754845fd2cb76d59ee7</h5>
         <div className={styles.codeBlock}>
           <pre>{JSON.stringify(gifById, null, 2)}</pre>
         </div>
